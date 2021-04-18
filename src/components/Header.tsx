68541dc3bb35e1014d44bb2f0001265e7e7ab98c
Chrisando Pramudhita <chrisep8@gmail.com>
2021-04-18 15:48:16 +0700

    change some styles.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react"
 
 const Header: FC = () => (
   <nav className="sticky left-0 right-0 top-0 justify-end z-50">
-    <div className={"max-w-6xl flex items-center mx-auto px-8 py-4 border-b bg-white bg-opacity-50 firefox:bg-opacity-90 backdrop-filter backdrop-blur-lg"}>
+    <div className={"max-w-6xl flex items-center mx-auto px-8 py-4 border-b bg-white bg-opacity-50 firefox:bg-opacity-90 backdrop-filter backdrop-filteblur"}>
       <Link to="/" className="flex items-center mr-auto">
         <img src={"/logo.svg"} alt="ant digital logo" className={"w-6 h-6"} />
         <p className="text-sm ml-3 font-bold font-body mb-1">Ant Digital</p>
