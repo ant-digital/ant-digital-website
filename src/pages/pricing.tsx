a99c7d515adb0a1cecdca58ca7d744c66b1383e7
Chrisando Pramudhita <chrsep@protonmail.com>
2021-05-04 20:48:03 +0700

    make pricing page responsive.

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -10,7 +10,7 @@ const Pricing = () => {
         </header>
       </div>
 
-      <div className="m-8 p-8 border border-primary border-2 rounded-xl flex">
+      <div className="m-8 p-8 border border-primary border-2 rounded-xl lg:flex">
         <div className={"flex-2 prose"}>
           <h2 className={"!mt-0 mr-auto"}>Business Websites</h2>
           <p>
@@ -24,7 +24,7 @@ const Pricing = () => {
             <li>Feature 4</li>
           </ul>
         </div>
-        <div className={"flex-1 prose border-l pl-8"}>
+        <div className={"flex-1 prose lg:border-l lg:pl-8 pt-8 lg:pt-0"}>
           <h3>Custom Price</h3>
           <p>
             Extend your website with custom feature and experiences tailored to
@@ -33,7 +33,7 @@ const Pricing = () => {
         </div>
       </div>
 
-      <div className="m-8 p-8 border border-green-400 border-2 rounded-xl flex">
+      <div className="m-8 p-8 border-green-400 border-2 rounded-xl border lg:flex ">
         <div className={"flex-2 prose"}>
           <h2>E-commerce</h2>
           <p>
@@ -47,13 +47,13 @@ const Pricing = () => {
             <li>Feature 4</li>
           </ul>
         </div>
-        <div className={"flex-1 prose border-l pl-8"}>
+        <div className={"flex-1 prose lg:border-l lg:pl-8 pt-8 lg:pt-0"}>
           <h3>Custom Price</h3>
           <p>understand your business/company/product.</p>
         </div>
       </div>
 
-      <div className="m-8 p-8 border border-red-400 border-2 rounded-xl flex">
+      <div className="m-8 p-8 border border-red-400 border-2 rounded-xl lg:flex">
         <div className={"flex-2 prose"}>
           <h2>Web Applications</h2>
           <p>
@@ -67,7 +67,7 @@ const Pricing = () => {
             <li>Feature 4</li>
           </ul>
         </div>
-        <div className={"flex-1 prose border-l pl-8"}>
+        <div className={"flex-1 prose lg:border-l lg:pl-8 pt-8 lg:pt-0"}>
           <h3>Custom Price</h3>
           <p>understand your business/company/product.</p>
         </div>
