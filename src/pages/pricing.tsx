80d9fad2e5f35d34e0078de8677dfca4f2c7466c
Chrisando Pramudhita <chrsep@protonmail.com>
2021-05-06 00:06:47 +0700

    use gradients on pricing page

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -10,66 +10,78 @@ const Pricing = () => {
         </header>
       </div>
 
-      <div className="m-8 p-8 border border-primary border-2 rounded-xl lg:flex">
-        <div className={"flex-2 prose"}>
-          <h2 className={"!mt-0 mr-auto"}>Business Websites</h2>
-          <p>
-            Build blazing fast informative websites that helps your customers
-            understand your business/company/product.
-          </p>
-          <ul>
-            <li>Feature 1</li>
-            <li>Feature 2</li>
-            <li>Feature 3</li>
-            <li>Feature 4</li>
-          </ul>
-        </div>
-        <div className={"flex-1 prose lg:border-l lg:pl-8 pt-8 lg:pt-0"}>
-          <h3>Custom Price</h3>
-          <p>
-            Extend your website with custom feature and experiences tailored to
-            your branding.
-          </p>
-        </div>
-      </div>
-
-      <div className="m-8 p-8 border-green-400 border-2 rounded-xl border lg:flex ">
-        <div className={"flex-2 prose"}>
-          <h2>E-commerce</h2>
-          <p>
-            Create blazing fast e-commerce website to build your own brand and
-            reach more diverse customers.
-          </p>
-          <ul>
-            <li>Feature 1</li>
-            <li>Feature 2</li>
-            <li>Feature 3</li>
-            <li>Feature 4</li>
-          </ul>
-        </div>
-        <div className={"flex-1 prose lg:border-l lg:pl-8 pt-8 lg:pt-0"}>
-          <h3>Custom Price</h3>
-          <p>understand your business/company/product.</p>
+      <div className="m-8 p-2 rounded-xl bg-gradient-to-br from-blue-700 to-purple-700">
+        <div className={"bg-white rounded-xl p-8 w-full lg:flex"}>
+          <div className={"flex-2 prose"}>
+            <h2 className={"!mt-0 mr-auto"}>Business Websites</h2>
+            <p>
+              Build blazing fast informative websites that helps your customers
+              understand your business/company/product.
+            </p>
+            <ul>
+              <li>Feature 1</li>
+              <li>Feature 2</li>
+              <li>Feature 3</li>
+              <li>Feature 4</li>
+            </ul>
+          </div>
+          <div className={"flex-1 prose lg:border-l lg:pl-8 pt-8 lg:pt-0"}>
+            <h3>Custom Price</h3>
+            <p>
+              Extend your website with custom feature and experiences tailored
+              to your branding.
+            </p>
+          </div>
         </div>
       </div>
 
-      <div className="m-8 p-8 border border-red-400 border-2 rounded-xl lg:flex">
-        <div className={"flex-2 prose"}>
-          <h2>Web Applications</h2>
-          <p>
-            Create blazing fast e-commerce website to build your own brand and
-            react more diverse customers.
-          </p>
-          <ul>
-            <li>Feature 1</li>
-            <li>Feature 2</li>
-            <li>Feature 3</li>
-            <li>Feature 4</li>
-          </ul>
+      <div className="m-8 p-2 rounded-xl bg-gradient-to-br from-emerald-600 to-green-700">
+        <div className={"bg-white rounded-xl p-8 w-full lg:flex"}>
+          <div className={"flex-2 prose"}>
+            <h2 className={"!mt-0 mr-auto"}>E-commerce</h2>
+            <p>
+              Build blazing fast informative websites that helps your customers
+              understand your business/company/product.
+            </p>
+            <ul>
+              <li>Feature 1</li>
+              <li>Feature 2</li>
+              <li>Feature 3</li>
+              <li>Feature 4</li>
+            </ul>
+          </div>
+          <div className={"flex-1 prose lg:border-l lg:pl-8 pt-8 lg:pt-0"}>
+            <h3>Custom Price</h3>
+            <p>
+              Extend your website with custom feature and experiences tailored
+              to your branding.
+            </p>
+          </div>
         </div>
-        <div className={"flex-1 prose lg:border-l lg:pl-8 pt-8 lg:pt-0"}>
-          <h3>Custom Price</h3>
-          <p>understand your business/company/product.</p>
+      </div>
+
+      <div className="m-8 p-2 rounded-xl bg-gradient-to-br from-rose-500 to-pink-700">
+        <div className={"bg-white rounded-xl p-8 w-full lg:flex"}>
+          <div className={"flex-2 prose"}>
+            <h2 className={"!mt-0 mr-auto"}>Web Applications</h2>
+            <p>
+              Build blazing fast informative websites that helps your customers
+              understand your business/company/product.
+            </p>
+            <ul>
+              <li>Feature 1</li>
+              <li>Feature 2</li>
+              <li>Feature 3</li>
+              <li>Feature 4</li>
+            </ul>
+          </div>
+          <div className={"flex-1 prose lg:border-l lg:pl-8 pt-8 lg:pt-0"}>
+            <h3>Custom Price</h3>
+            <p>
+              Extend your website with custom feature and experiences tailored
+              to your branding.
+            </p>
+          </div>
         </div>
       </div>
     </Layout>
